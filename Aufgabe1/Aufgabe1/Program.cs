5762ea88444ed287e4401ad3b5ab4a9ca4945f29
ifw16422 <Michael-Stuehler@web.de>
2017-05-04 15:47:38 +0200

    first commit

---
+++ Aufgabe1/Aufgabe1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+
+            int n_1000 = 0;
+            int n_10000 = 0;
+            int n_100000 = 0;
+
+            Vector2[] vectors = reader.readFromFileToArray(@"S:\CG\Aufgabe1\Aufgabe1\s_1000_1.dat");
+            DateTime start_1000 = DateTime.Now;
+            for(int i = 0; i < vectors.Length - 1; i++)
+            {
+                for(int j = i + 1; j < vectors.Length; j++)
+                {
+                    if (vectors[i].CheckForIntersection(vectors[i], vectors[j]))
+                    {
+                        n_1000++;
+                    }
+                }
+            }
+            DateTime end_1000 = DateTime.Now;
+            Console.WriteLine("Intersections at s_1000_1: " + n_1000 + " Time needed (ms): " + (end_1000 - start_1000).Milliseconds);
+
+            vectors = reader.readFromFileToArray(@"S:\CG\Aufgabe1\Aufgabe1\s_10000_1.dat");
+            DateTime start_10000 = DateTime.Now;
+            for (int i = 0; i < vectors.Length - 1; i++)
+            {
+                for (int j = i + 1; j < vectors.Length; j++)
+                {
+                    if (vectors[i].CheckForIntersection(vectors[i], vectors[j]))
+                    {
+                        n_10000++;
+                    }
+                }
+            }
+            DateTime end_10000 = DateTime.Now;
+            Console.WriteLine("Intersections at s_10000_1: " + n_10000 + " Time needed (ms): " + (end_10000 - start_10000).Milliseconds);
+
+            vectors = reader.readFromFileToArray(@"S:\CG\Aufgabe1\Aufgabe1\s_100000_1.dat");
+            DateTime start_100000 = DateTime.Now;
+            for (int i = 0; i < vectors.Length - 1; i++)
+            {
+                for (int j = i + 1; j < vectors.Length; j++)
+                {
+                    if (vectors[i].CheckForIntersection(vectors[i], vectors[j]))
+                    {
+                        n_100000++;
+                    }
+                }
+            }
+            DateTime end_100000 = DateTime.Now;
+            Console.WriteLine("Intersections at s_100000_1: " + n_100000 + " Time needed (ms): " + (end_100000 - start_100000).Milliseconds);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aufgabe1/Aufgabe1/Reader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe1
+{
+    class Reader
+    {
+        public List<Vector2> readFromFileToList(string path)
+        {
+            List<Vector2> vectors = new List<Vector2>();
+
+            try
+            {
+                // Open the text file using a stream reader.
+                Vector2 vector;
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    String line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        String[] values = line.Split(' ');
+                        double sx = Double.Parse(values[0], CultureInfo.InvariantCulture);
+                        double sy = Double.Parse(values[1], CultureInfo.InvariantCulture);
+                        double ex = Double.Parse(values[2], CultureInfo.InvariantCulture);
+                        double ey = Double.Parse(values[3], CultureInfo.InvariantCulture);
+                        vector = new Vector2(sx, sy, ex, ey);
+
+                        vectors.Add(vector);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return vectors;
+        }
+
+        public Vector2[] readFromFileToArray(string path)
+        {
+            Vector2[] vectors = new Vector2[File.ReadLines(path).Count()];
+            int cnt = 0;
+
+            try
+            {
+                // Open the text file using a stream reader.
+                Vector2 vector;
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    String line;
+
+                    // Read the stream to a string, and write the string to the console.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        String[] values = line.Split(' ');
+                        double sx = Double.Parse(values[0], CultureInfo.InvariantCulture);
+                        double sy = Double.Parse(values[1], CultureInfo.InvariantCulture);
+                        double ex = Double.Parse(values[2], CultureInfo.InvariantCulture);
+                        double ey = Double.Parse(values[3], CultureInfo.InvariantCulture);
+                        vector = new Vector2(sx, sy, ex, ey);
+
+                        vectors[cnt++] = vector;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return vectors;
+        }
+    }
+}
