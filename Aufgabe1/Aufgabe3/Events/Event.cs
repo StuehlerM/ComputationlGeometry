e29e763935d0b96ffd9a35b89214bc9ea61e89d4
Florian Kraus <fkraus@hm.edu>
2017-05-23 13:15:02 +0200

    git in Visual Studio ist kackeeee

---
+++ Aufgabe1/Aufgabe3/Events/Event.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe3
+{
+    enum EventType { begin, end, intersect };
+
+    class Event 
+    {
+
+        public Event(EventType type, Vector2 line)
+        {
+            this.eventType = type;
+            this.line_1 = line;
+            this.line_2 = null;
+        }
+
+        public Event(EventType type, Vector2 line_1, Vector2 line_2)
+        {
+            this.eventType = type;
+            this.line_1 = line_1;
+            this.line_2 = line_2;
+        }
+
+        public EventType eventType { get; }
+        public Vector2 line_1 { get; }
+        public Vector2 line_2 { get; }
+
+        public Point getPoint()
+        {
+            switch (eventType)
+            {
+                case EventType.begin:
+                    return line_1.start;
+                case EventType.end:
+                    return line_1.end;
+                case EventType.intersect:
+                    /*double delta = line_1.start * line_2.end - line_1.end* line_2.start;
+                    if (delta == 0)
+                        throw new ArgumentException("Lines are parallel");
+
+                    float x = (B2 * C1 - B1 * C2) / delta;
+                    float y = (A1 * C2 - A2 * C1) / delta;
+
+                    return line_1.start;*/
+                    break;
+                default:
+                    break;
+            }
+            return null;
+        }
+
+        private void calculateIntersection()
+        {
+            double slope_1 = (line_1.end.y - line_1.start.y) / (line_1.end.x - line_1.start.x);
+            double slope_2 = (line_2.end.y - line_2.start.y) / (line_2.end.x - line_2.start.x);
+
+        }
+    }
+}
---
+++ Aufgabe1/Aufgabe3/Vector2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe3
+{
+    class Vector2
+    {
+        public Point start { get; set; }
+        public Point end { get; set; }
+
+        //public static int insiders = 0;
+
+        public Vector2(double sx, double sy, double ex, double ey)
+        {
+            if (sx < ex)
+            {
+                start = new Point(sx, sy);
+                end = new Point(ex, ey);
+            }
+            else
+            {
+                end = new Point(sx, sy);
+                start = new Point(ex, ey);
+            }
+        }
+
+        public bool CheckForIntersection(Vector2 v1, Vector2 v2)
+        {
+            bool intersect = false;
+
+            double ccw_v1_start = ccw(v1.start, v1.end, v2.start);
+            double ccw_v1_end = ccw(v1.start, v1.end, v2.end);
+
+            double ccw_v2_start = ccw(v2.start, v2.end, v1.start);
+            double ccw_v2_end = ccw(v2.start, v2.end, v1.end);
+
+            if(ccw_v1_start == 0 && ccw_v1_end == 0)
+            {
+                if(CheckInside(v1, v2.start) || CheckInside(v1, v2.end))
+                {
+                    intersect = true;
+                }
+            }
+            else if((ccw_v1_start * ccw_v1_end <= 0) && (ccw_v2_start * ccw_v2_end <= 0))
+            {
+                intersect = true;
+            }
+            
+            return intersect;
+        }
+
+        double ccw(Point p, Point q, Point r)
+        {
+            return ((p.x*q.y - p.y*q.x) + (q.x * r.y - q.y * r.x) + (p.y * r.x - p.x * r.y));
+        }
+
+        bool CheckInside(Vector2 v, Point p)
+        {
+            bool inside = false;
+            double EPSILON = Math.Pow(10, -15);
+
+            double slope = ((v.end.y - v.start.y) / (v.end.x - v.start.x));
+            double yAxis = v.start.y - slope * v.start.x;
+
+            if( Math.Abs(p.y - (slope*p.x + yAxis)) < EPSILON)
+            {
+                inside = true;
+            }
+
+            return inside;
+        }
+
+        public override string ToString()
+        {
+            return "Startpunkt: " + start.ToString() +  " Ende: " + end.ToString();
+        }
+    }
+}
